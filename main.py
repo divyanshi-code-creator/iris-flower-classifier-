2c469c3842f8d9906633b1db471e755aa1827add
Divyanshi Mohanty <divyanshi.25bce10828@vitbhopal.ac.in>
2026-03-31 20:10:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, accuracy_score
+
+url  = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+column_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
+iris_data = pd.read_csv(url, names=column_names)
+
+iris_data.head()
